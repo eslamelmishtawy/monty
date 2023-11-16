9ae4bc59f4940a0145d9fb1e5d6d114e3711bafa
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-11-16 11:48:13 -0800

    dsad

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+
+int main(int argc, char **argv)
+{
+	stack_t stack = NULL;
+	FILE *fd = NULL;
+	char *line = NULL;
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FALIURE);
+	}
+
+}
